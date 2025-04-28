be101f502c8caf163eed80fbf724537ea4595332
gabriel.rodrigues <fgrodrigues.gabriel@gmail.com>
2025-04-28 10:50:08 -0300

    :tada: Iniciando projeto Hackaton

---
+++ src/main/java/com/hackaton/equipeum/EquipeumApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.equipeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipeumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/equipeum/EquipeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.equipeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
